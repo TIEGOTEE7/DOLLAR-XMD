ad833371c7456c88ccfc320e5d893fdc7e252d86
TIEGOTEE7 <phashatiego123@gmail.com>
2025-08-08 23:25:39 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Dollar Bot',
+  author: '‎',
+  botName: "dollar Bot",
+  botOwner: 'Shadow Tee', // Your name
+  ownerNumber: '+27712943636', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
